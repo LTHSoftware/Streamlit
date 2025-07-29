fed411679f36426b130431a29fc74b7b9fabfdd4
LTHSoftware <78642126+LTHSoftware@users.noreply.github.com>
2025-07-29 14:27:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ with st.sidebar:
 # --- Inicializace navigace ---
 nav = st.navigation({
     "": st.Page(page_home, title="  Domů"),
-    "": []
+    "": [],
     "Navigace": pages,
 }, position="sidebar", expanded=True)
 # --- Spuštění vybrané stránky ---
