cc75e073192fe192ce2cfa12624c64001a41c799
LTHSoftware <78642126+LTHSoftware@users.noreply.github.com>
2025-07-29 12:41:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ colors = {
 }
 
 # Vytvoř dva sloupce
-col1, col2 = st.columns([1, 5])
+col1, col2 = st.columns([1, 1])
 
 with col1:
     selected_color_name = st.selectbox(" ", list(colors.keys()), label_visibility="collapsed")
