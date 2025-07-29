a5dc96eb5565ebc4746f0206f3e65217aba037f6
LTHSoftware <78642126+LTHSoftware@users.noreply.github.com>
2025-07-29 14:44:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,3 +66,11 @@ nav = st.navigation({
 }, expanded=True)
 # --- Spuštění vybrané stránky ---
 nav.run()
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
